474d1b8ec8a55c4a0786548a4d7a0ce7881638e6
MacBook Pro <macbookpro@MacBooks-MBP.lan>
2023-04-09 19:06:05 +0700

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,20 @@
-import './globals.css'
+import { Nunito } from "@next/font/google";
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+
+export const metadata = {
+  title: "AirBnB Clone",
+  description: "AirBnB Clone",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +23,10 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
